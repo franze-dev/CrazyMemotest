2e2add0076852511f7942a51bdc76dfec3cd6b7a
Nicolas Leon <nicolasaleon05@gmail.com>
2024-11-21 16:37:15 -0300

    Add game.h and game.cpp

---
+++ memotest/src/objects/game.cpp
@@ -0,0 +1,77 @@
+#include "game.h"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+
+using namespace sf;
+
+namespace Memotest
+{
+	static RenderWindow* window;
+	Sprite cardSprite;
+
+	static void Load();
+	static void Init();
+	static void Update();
+	static void Draw();
+	static void Unload();
+	static void Close();
+	
+
+	void Memotest::Run()
+	{
+		Load();
+		Init();
+
+		do
+		{
+			Update();
+			Draw();
+
+		} while (true);
+
+		Close();
+	}
+
+	void Load()
+	{
+
+	}
+
+	void Init()
+	{
+		window = new RenderWindow(VideoMode(800, 600), "Crazy Memotest");
+	}
+
+	void Update()
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		Event event;
+		while (window->pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == Event::Closed)
+				window->close();
+		}
+
+	}
+
+	void Draw()
+	{
+	
+		window->display();
+	}
+
+	void Unload()
+	{
+
+	}
+
+	void Close()
+	{
+		delete window;
+		Unload();
+	}
+
+}
+
+
---
+++ memotest/src/objects/game.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Memotest
+{
+	void Run();
+};
+
